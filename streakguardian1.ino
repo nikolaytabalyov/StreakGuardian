fd7ecfbb747a61c8d7bc2e9d74c04823c839073c
DaniDagami <daniel.g.boychev@gmail.com>
2024-04-20 16:27:25 +0300

    Music & Timer

--- streakguardian1.ino
+++ streakguardian1.ino
@@ -1,39 +1,58 @@
 // include the library code:
 #include <LiquidCrystal.h>
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+
+#define C4 261
+#define D4 293
+#define E4 329
+#define F4 349
+#define G4 392
+#define A4 440
+#define B4 493
+#define C5 523
+#define E5 659
+
+#define QUARTER_NOTE 400
+#define HALF_NOTE 800
+#define EIGHTH_NOTE 200
+
+const unsigned long interval = 86400000; // Interval in milliseconds (e.g., 60000 ms = 1 minute)
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int i = 7;
-
 int buttonStateOne = 0;
 int buttonStateTwo = 0;
 int buttonStateThree = 0;
+int buttonStateFour = 0;
+int buttonStateFive = 0;
 
 int currentIndex = 0;
 
-const int maxLength = 10;    // Maximum length of the arrays
-String taskArray[maxLength]; // Array to hold tasks
-int streakArray[maxLength];  // Array to hold streaks
-int currentLength = 0;       // Variable to keep track of current length
+const int maxLength = 10;           // Maximum length of the arrays
+String taskArray[maxLength];        // Array to hold tasks
+int streakArray[maxLength];         // Array to hold streaks
+long startedStreakTimes[maxLength]; // The times when the task has last been done
+int currentLength = 0;              // Variable to keep track of current length
 
 void setup() {
-    addTask("Cook");
     Serial.begin(9600);
-    // set up the LCD's number of columns and rows:
+
     lcd.begin(16, 2);
 
+    pinMode(6, INPUT);
+    pinMode(7, INPUT);
     pinMode(8, INPUT);
     pinMode(9, INPUT);
     pinMode(10, INPUT);
+    pinMode(13, OUTPUT);
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
 
+    buttonStateFour = digitalRead(7);
+    buttonStateFive = digitalRead(6);
     buttonStateThree = digitalRead(8);
     buttonStateTwo = digitalRead(9);
     buttonStateOne = digitalRead(10);
@@ -46,6 +65,8 @@ void loop() {
     lcd.print(currentTask);
     lcd.setCursor(0, 1);
     lcd.print(currentStreak);
+
+    checkTaskTimers();
 
     /*
       lcd.setCursor(0, 0);
@@ -72,15 +93,13 @@ void loop() {
 
     */
 
-    // Button 1
-    if (buttonStateOne == HIGH) {
-        // turn LED on
-        digitalWrite(LED_BUILTIN, HIGH);
+    // Button 1 (Previous Task Button)
+    if (buttonStateOne == HIGH) { // Button = Pressed
 
         currentIndex--;
 
         if (currentIndex == -1) {
-            currentIndex = 3 - 1;
+            currentIndex = currentLength - 1;
         }
 
         currentTask = taskArray[currentIndex];
@@ -88,34 +107,34 @@ void loop() {
         currentStreak = streakArray[currentIndex];
     }
 
-    // Button 2
-    if (buttonStateTwo == HIGH) {
-        // turn LED on
-        digitalWrite(LED_BUILTIN, HIGH);
+    // Button 2 (Next Task Button)
+    if (buttonStateTwo == HIGH) { // Button = Pressed
 
         currentIndex++;
 
-        if (currentIndex == 3) {
+        if (currentIndex == currentLength) {
             currentIndex = 0;
         }
 
         currentTask = taskArray[currentIndex];
 
         currentStreak = streakArray[currentIndex];
-    } else {
-        // turn LED off
-        digitalWrite(LED_BUILTIN, LOW);
     }
 
-    // Button 3
-    if (buttonStateThree == HIGH) {
-        // turn LED on
-        digitalWrite(LED_BUILTIN, HIGH);
-
+    // Button 3 (Increase Streak Counter Button)
+    if (buttonStateThree == HIGH) { // Button = Pressed
         streakArray[currentIndex]++;
-    } else {
-        // turn LED off
-        digitalWrite(LED_BUILTIN, LOW);
+        startedStreakTimes[currentIndex] = millis();
+    }
+
+    // Button 4 (Add Task Button)
+    if (buttonStateFour == HIGH) {
+        addTask();
+    }
+
+    // Button 5 (Remove Task Button)
+    if (buttonStateFive == HIGH) { // Button = Pressed
+        removeTask();
     }
 
     delay(120); // Delay a little bit to improve simulation performance
@@ -125,7 +144,17 @@ int getLength() {
     return sizeof(streakArray) / sizeof(streakArray[0]);
 }
 
-void addTask(String newTask) {
+void addTask() {
+
+    Serial.println("Enter new task:");
+    while (Serial.available() == 0) {
+    }
+
+    // Get task name from user
+    String newTask = Serial.readString();
+    newTask.trim();
+    Serial.println(newTask);
+
     // Check if there's space to add a new task
     if (currentLength >= maxLength) {
         Serial.println("Cannot add more tasks, array is full!");
@@ -135,12 +164,19 @@ void addTask(String newTask) {
     // Add the new task to the next empty slot in the arrays
     taskArray[currentLength] = newTask;
     streakArray[currentLength] = 0;
-
+    startedStreakTimes[currentLength] = millis();
     // Increment the current length
     currentLength++;
 }
 
-void remove() {
+void removeTask() {
+    // Display available tasks to remove
+    for (int i = 0; i < currentLength; i++) {
+        Serial.println(i + 1 + taskArray[i]);
+    }
+    Serial.println("Enter id to remove task:");
+    int id = Serial.parseInt();
+
     // Check if there are tasks to remove
     if (currentLength == 0) {
         Serial.println("No tasks to remove!");
@@ -148,9 +184,69 @@ void remove() {
     }
 
     // Remove the last task added
-    taskArray[currentLength - 1] = "";
-    streakArray[currentLength - 1] = 0;
+    taskArray[id - 1] = "";
+    streakArray[id - 1] = 0;
+
+    // Shift all elements after the removed element one index left
+    for (int i = id; i < currentLength - 1; i++) {
+        taskArray[i] = taskArray[i + 1];
+        streakArray[i] = streakArray[i + 1];
+    }
 
     // Decrement the current length
     currentLength--;
+}
+
+// Function to play a note with a specific duration
+void playNote(int frequency, int duration) {
+    tone(13, frequency, duration); // Play tone on pin 8
+    delay(duration);               // Add a small delay for spacing between notes
+}
+
+void TetrisTheme() {
+    // Play Tetris theme melody
+    playNote(E4, QUARTER_NOTE);
+    playNote(B4, QUARTER_NOTE);
+    playNote(C5, HALF_NOTE);
+    playNote(A4, EIGHTH_NOTE);
+    playNote(G4, EIGHTH_NOTE);
+    playNote(F4, HALF_NOTE);
+
+    playNote(E4, QUARTER_NOTE);
+    playNote(B4, QUARTER_NOTE);
+    playNote(C5, HALF_NOTE);
+    playNote(A4, EIGHTH_NOTE);
+    playNote(G4, EIGHTH_NOTE);
+    playNote(F4, HALF_NOTE);
+
+    playNote(E4, QUARTER_NOTE);
+    playNote(B4, QUARTER_NOTE);
+    playNote(C5, HALF_NOTE);
+    playNote(E5, EIGHTH_NOTE);
+    playNote(C5, EIGHTH_NOTE);
+    playNote(B4, HALF_NOTE);
+
+    playNote(A4, QUARTER_NOTE);
+    playNote(F4, QUARTER_NOTE);
+    playNote(G4, HALF_NOTE);
+    playNote(E4, EIGHTH_NOTE);
+    playNote(G4, EIGHTH_NOTE);
+    playNote(A4, HALF_NOTE);
+
+    delay(1000);
+}
+
+void checkTaskTimers() {
+    // Get the current time
+    unsigned long currentMillis = millis();
+    // Serial.println(currentMillis);
+    for (int i = 0; i < currentLength; i++) {
+        // Serial.println(startedStreakTimes[i]);
+        if (currentMillis - startedStreakTimes[i] >= interval && streakArray[i] != 0) { // Check if timer has ran out
+            streakArray[i] = 0;                                                         // Decrease the streak
+            // Streak not increased in time, trigger EyeOfTheTiger()
+            TetrisTheme();
+            Serial.println("Timer ran out!!!" + taskArray[i]);
+        }
+    }
 }
